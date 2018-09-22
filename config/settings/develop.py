05135d671bc84b1a5b117fd2967b5a311cdd64c1
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-09-21 19:06:46 -0500

    first commit

---
+++ config/settings/develop.py
@@ -0,0 +1,21 @@
+# config/settings/develop.py
+import os
+
+from .common import *
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+THIRD_PARTY_APPS_LOCAL = (
+)
+
+INSTALLED_APPS += THIRD_PARTY_APPS_LOCAL
