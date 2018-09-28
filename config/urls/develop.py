5149863b7525bd14fadfb80be12b5965517c3c27
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-09-28 18:05:51 -0500

    DELETE unused files

--- config/urls/develop.py
+++
@@ -1,8 +0,0 @@
-# config/urls/develop.py
-from .common import *
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
