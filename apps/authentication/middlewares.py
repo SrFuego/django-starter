dabd8447639b5513e19f698d88f1db039c2d7eee
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-10-02 12:28:06 -0500

    FIX formart file middlewares

--- apps/authentication/middlewares.py
+++ apps/authentication/middlewares.py
@@ -1,9 +1,20 @@
+# apps/authentication/middlewares.py
+# Python imports
+
+
+# Django imports
+
+
+# Third party apps imports
 from rest_framework.authentication import TokenAuthentication
 
 
+# Local imports
+
+
+# Create your middlewares here.
 class TokenAuthenticationMiddleware(object):
     def resolve(self, next, root, info, **args):
-
         if root is None:
             token_exempt = getattr(next.args[0], 'token_exempt', False)
             is_introspection = getattr(
