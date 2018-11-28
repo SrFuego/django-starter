93b9dd83a0e6fe22a2a31a53763a98cdfd076944
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-11-27 19:56:56 -0500

    FIX rest project branch

--- config/urls/common.py
+++ config/urls/common.py
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
+
+from rest_framework.documentation import include_docs_urls
+
+from apps.core.routers import router
+
+API_TITLE = '{{ project_name }}'
+API_DESCRIPTION = '...'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        r'api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')),
+    path(
+        r'docs/',
+        include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
+    path(r'api/v1/', include(router.urls)),
 ]
