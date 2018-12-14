46dfeca32d401fce03b154c4d694bf91edaa2ecf
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-12-13 20:47:13 -0500

    FIX change to simple quote

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 # apps/{{ app_name }}/__init__.py
-default_app_config = "apps.{{ app_name }}.apps.{{ app_name | title }}Config"
+default_app_config = 'apps.{{ app_name }}.apps.{{ app_name | title }}Config'
