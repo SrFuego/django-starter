aa7c8de20f776c7043bfb6bed305bd773db698aa
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-12-13 20:39:23 -0500

    FIX change simple quotes

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 # CORS_ORIGIN_WHITELIST = (
-#     "google.com",
-#     "hostname.example.com",
-#     "localhost:8000",
-#     "127.0.0.1:9000",)
+#     'google.com',
+#     'hostname.example.com',
+#     'localhost:8000',
+#     '127.0.0.1:9000',)
