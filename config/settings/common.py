af5bb27ad72a484b91c4eb6e3b497078ef9d73b2
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-12-14 23:33:47 -0500

    ADD cors headers configuration

--- config/settings/common.py
+++ config/settings/common.py
@@ -39,6 +39,7 @@ DJANGO_APPS = (
 )
 
 THIRD_PARTY_APPS = (
+    'corsheaders',
 )
 
 LOCAL_APPS = (
@@ -51,6 +52,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
