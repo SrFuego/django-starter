3a111e4712c560516c43e481e3915eb982bbc023
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-12-14 23:34:47 -0500

    FIX testing settings

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -12,31 +12,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-THIRD_PARTY_APPS_LOCAL = (
-    'django_extensions',
-    'speedinfo',
-)
-
-INSTALLED_APPS += THIRD_PARTY_APPS_LOCAL
 
 # Django Rest Framework CORS configuration
 CORS_ORIGIN_ALLOW_ALL = True
-
-# Graph models conf
-GRAPH_MODELS = {
-    'all_applications': True,
-    'group_models': True,
-}
-
-# Django Speedinfo conf
-MIDDLEWARE += [
-    'speedinfo.middleware.ProfilerMiddleware',
-]
-
-CACHES = {
-    'default': {
-        'BACKEND': 'speedinfo.backends.proxy_cache',
-        'CACHE_BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': '/var/tmp/django_cache',
-    }
-}
